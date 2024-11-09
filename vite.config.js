7f58ebba339280f090d4bd3beddc1c515b934a79
bananchiki7 <155952002+bananchiki7@users.noreply.github.com>
2024-11-09 22:12:13 +0200

    create repository

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
+
+export default defineConfig({
+  root: "src",
+
+  build: {
+    target: ["es2022", "chrome89", "firefox89", "safari15"],
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        // nested: resolve(__dirname, "src/info.html")
+        // nestedTwo: resolve(__dirname, "src/profile.html"),
+        // nestedFour: resolve(__dirname, "src/advert.html"),
+      },
+    },
+    outDir: "../dist",
+  },
+  plugins: [injectHTML(), FullReload(["./src//.html"])],
+});
