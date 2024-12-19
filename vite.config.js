c55bd5be7ba32421389289a7c137cc77d8997cb1
bananchiki7 <155952002+bananchiki7@users.noreply.github.com>
2024-12-19 13:51:35 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,19 @@ import injectHTML from "vite-plugin-html-inject";
 import FullReload from "vite-plugin-full-reload";
 
 export default defineConfig({
-  base: '/it-wave/',
+  // base: '/it-wave/',
   root: "src",
-
+  // server: {
+  //   hmr: {
+  //     overlay: false,
+  //   },
+  // },
   build: {
     target: ["es2022", "chrome89", "firefox89", "safari15"],
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        // nested: resolve(__dirname, "src/info.html")
+        // nested: resolve(__dirname, "src/hobbies.html"),
         // nestedTwo: resolve(__dirname, "src/profile.html"),
         // nestedFour: resolve(__dirname, "src/advert.html"),
       },
