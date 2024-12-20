cb744b3eee42cfd81d3a4f7f86320f762de2a5b9
bananchiki7 <155952002+bananchiki7@users.noreply.github.com>
2024-12-20 23:57:54 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        // nested: resolve(__dirname, "src/hobbies.html"),
+        nested: resolve(__dirname, "src/hobby.html"),
         // nestedTwo: resolve(__dirname, "src/profile.html"),
         // nestedFour: resolve(__dirname, "src/advert.html"),
       },
