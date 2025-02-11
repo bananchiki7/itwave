2497a8b556c4780300acf6474b9a79f3c8754df6
bananchiki7 <155952002+bananchiki7@users.noreply.github.com>
2025-02-11 10:23:57 +0000

    Deploying to gh-pages from @ bananchiki7/itwave@e2a23c2d5018f489e8cac0557ce08ff34f1b26cc 🚀

---
+++ assets/mainLoader-Bq2P0Tnw.js
@@ -0,0 +1,1 @@
+import{e}from"./bodyLock-CXhmA4vK.js";const o=document.querySelector(".main-loader-wrapper");function a(){setTimeout(()=>{o.classList.add("is-hidden"),e()},1e3)}export{a as r};
--- assets/mainLoader-vDGbj-O4.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./bodyLock-DTHPdllp.js";const o=document.querySelector(".main-loader-wrapper");function a(){setTimeout(()=>{o.classList.add("is-hidden"),e()},1e3)}export{a as r};
